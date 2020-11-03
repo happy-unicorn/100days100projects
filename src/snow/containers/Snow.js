57540cf44499fdde115e84c8f3a4de9bfcbe134f
Michael Chernomazov <michael23black@yandex.ru>
2020-11-03 23:53:35 +0300

    Completed snow animation

--- src/snow/containers/Snow.js
+++ src/snow/containers/Snow.js
@@ -2,21 +2,27 @@ import React, {useCallback, useEffect, useRef} from 'react';
 import styled, { keyframes } from 'styled-components';
 import snowflake from '../images/snowflake.png';
 
+const snowflakeKeyframes = keyframes`
+  0% {
+    top: -5%;
+  }
+  100% {
+    top: 105%;
+  }
+`;
 const Container = styled.div`
   position: relative;
   height: 100%;
   width: 100%;
   overflow: hidden;
+  
   & div {
-    width: 30px;
-    height: 100%;
-    color: #fff;
     position: absolute;
-    top: -20px;
-    animation: fall 5s linear forwards;
-    background-image: url('https://img.icons8.com/fluent/72/popcorn.png');
+    top: -5vh;
+    background-image: url(${snowflake});
     background-size: 100%;
-    backface-visibility: visible;
+    background-repeat: no-repeat;
+    animation: ${snowflakeKeyframes} linear forwards;
   }
 `;
 const Snow = () => {
@@ -24,39 +30,28 @@ const Snow = () => {
 
     const createSnowflake = useCallback(() => {
         const snowflake = document.createElement('div');
+        const { width } = container.current.getBoundingClientRect();
 
+        const size = Math.random() * 30 + 30 + 'px';
+        snowflake.style.width = size;
+        snowflake.style.height = size;
+
+        snowflake.style.left = Math.random() * width + 'px';
+        snowflake.style.animationDuration = Math.random() * 5 + 5 + 's';
+        snowflake.style.opacity = Math.random();
+
+        container.current.appendChild(snowflake);
+
+        setTimeout(() => {
+            snowflake.remove();
+        }, 10000);
     }, [container]);
-    function createSnowFlake() {
-	// Creating the <i> tag
-	const snow_flake = document.createElement('div');
-	// Adding the required classes for the FontAwesome icon to show up
-	snow_flake.classList.add('far');
-	snow_flake.classList.add('fa-snowflake');
 
-	// Randomly generate the width to be between 10 and 20 px
-	snow_flake.style.width = Math.random() * 10 + 10 + 'px';
-	snow_flake.style.height = Math.random() * 10 + 10 + 'px';
-
-	// Randomly generate the left position to be between 0 and the innerWidth of the screen
-	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
-
-	// Randomly generate the animationDuration - between 2 and 5 seconds
-	snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's';
-
-	// Randomly add an opacity - between 0 and 1
-	snow_flake.style.opacity = Math.random();
-
-	// Add the newly created <i> tag inside the <body> tag
-	container.current.appendChild(snow_flake);
-
-	// Set a timeout to remove the snow_flake from the DOM after 5 seconds
-	// as we don't want it to overload the page
-	setTimeout(() => {
-		snow_flake.remove();
-	}, 5000);
-}
     useEffect(() => {
-        setInterval(createSnowFlake, 100);
+        const spammer = setInterval(createSnowflake, 10);
+        return () => {
+            clearInterval(spammer);
+        };
     }, []);
 
     return (
--- src/snow/index.js
+++ src/snow/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Snow from './containers/Snow';
-import './index.css';
 
 const Page = styled.div`
   display: flex;
