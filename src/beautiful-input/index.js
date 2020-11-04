5566b987a50fb65f85ca65de4f06fd12b4984145
Michael Chernomazov <michael23black@yandex.ru>
2020-11-04 21:31:03 +0300

    Implemented beautiful input

--- src/beautiful-input/index.js
+++ src/beautiful-input/index.js
@@ -1,14 +1,73 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import styled from 'styled-components';
+import { Formik } from 'formik';
+import Input from './components/Input';
+import { themes } from './styles/themes';
 
 const Page = styled.div`
+  display: flex;
   height: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.background.color.main};
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  align-items: center;
+  justify-content: center;
+  max-width: 550px;
+  padding-top: 30px;
+  border-radius: ${props => props.theme.border.radius.standard};
+  background-color: ${props => props.theme.background.color.main};
+  box-shadow: 
+    ${props => props.theme.shadow.direction.positive} ${props => props.theme.shadow.color.dark}, 
+    ${props => props.theme.shadow.direction.negative} ${props => props.theme.shadow.color.light};
+  & > div {
+    width: 80%;
+    padding: 0 40px;
+  }
+`;
+const Switch = styled.span`
+  display: flex;
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  width: 50px;
+  border-radius: ${props => props.theme.border.radius.standard};
+  background-color: ${props => props.theme.background.color.main};
+  box-shadow: 
+    ${props => props.theme.shadow.direction.positive} ${props => props.theme.shadow.color.dark}, 
+    ${props => props.theme.shadow.direction.negative} ${props => props.theme.shadow.color.light};
+  font-family: ${props => props.theme.text.font.bold};
+  font-size: ${props => props.theme.text.size.big};
+  color: ${props => props.theme.text.color.main};
 `;
 
-const BeautifulInputPage = (props) => {
+const BeautifulInputPage = () => {
+    const [theme, setTheme] = useState('dark');
+
+    const themeSwitchHandler = useCallback(() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    }, [theme, setTheme]);
+
     return (
-        <Page>
-            хе хе
+        <Page theme={themes[theme]}>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+            >
+                {(props) => (
+                    <Form noValidate theme={themes[theme]}>
+                        <Switch theme={themes[theme]} onClick={themeSwitchHandler}>T</Switch>
+                        <Input type={'email'} name={'email'} placeholder={'Email'} error={'help'} theme={themes[theme]}/>
+                        <Input type={'password'} name={'password'} placeholder={'Password'} theme={themes[theme]}/>
+                    </Form>
+                )}
+            </Formik>
         </Page>
     );
 };
---
+++ src/beautiful-input/styles/themes.js
@@ -0,0 +1,108 @@
+export const themes ={
+    // Dark Theme
+    dark: {
+        background: {
+            color: {
+                main: '#2f343f'
+            }
+        },
+        shadow: {
+            direction: {
+                negative: '-2px -2px 10px',
+                straight: '0 0 10px',
+                positive: '3px 3px 10px'
+            },
+            color: {
+                light: '#404552',
+                dark: '#1d2128',
+                error: '#ff1c1c'
+            }
+        },
+        indicator: {
+            color: {
+                standard: '#2f343f',
+                active: '#9aab98'
+            }
+        },
+        border: {
+            radius: {
+                standard: '20px'
+            },
+            color: {
+                main: '#2f343f'
+            }
+        },
+        text: {
+            size: {
+                smaller: '12px',
+                small: '15px',
+                medium: '20px',
+                big: '25px'
+            },
+            font: {
+                light: '"Nunito Light", sans-serif',
+                regular: '"Nunito Regular", sans-serif',
+                bold: '"Nunito Bold", sans-serif'
+            },
+            color: {
+                main: '#c1c1c1',
+                placeholder: '#969799',
+                success: '#9aab98',
+                error: '#733939'
+            }
+        }
+    },
+    // Light Theme
+    light: {
+        background: {
+            color: {
+                main: '#e0e5ec'
+            }
+        },
+        shadow: {
+            direction: {
+                negative: '-2px -2px 10px',
+                straight: '0 0 6px',
+                positive: '3px 3px 10px'
+            },
+            color: {
+                light: '#ffffff',
+                dark: '#a3b1c6',
+                error: '#fb0044'
+            }
+        },
+        indicator: {
+            color: {
+                standard: '#e0e5ec',
+                active: '#9aab98'
+            }
+        },
+        border: {
+            radius: {
+                standard: '20px'
+            },
+            color: {
+                main: '#e0e5ec'
+            }
+        },
+        text: {
+            size: {
+                smaller: '12px',
+                small: '15px',
+                medium: '20px',
+                big: '25px'
+            },
+            font: {
+                light: '"Nunito Light", sans-serif',
+                regular: '"Nunito Regular", sans-serif',
+                bold: '"Nunito Bold", sans-serif'
+            },
+            color: {
+                main: '#404552',
+                placeholder: '#969799',
+                success: '#9aab98',
+                error: '#ab4646'
+            }
+        }
+    }
+};
