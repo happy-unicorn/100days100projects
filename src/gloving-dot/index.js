767acfc68c7ebfcefa4651f4d105e543925f8af3
Michael Chernomazov <michael23black@yandex.ru>
2021-02-27 00:00:18 +0300

    Added duration mode

--- src/gloving-dot/index.js
+++ src/gloving-dot/index.js
@@ -39,8 +39,8 @@ const Dot = styled.span`
     background-color: #00ff0a;
     box-shadow: 0 0 10px #00ff0a, 0 0 20px #00ff0a, 0 0 40px #00ff0a, 0 0 60px #00ff0a, 0 0 80px #00ff0a, 0 0 100px #00ff0a;
     pointer-events: none;
-    transform: scale(${({ amode }) => amode ? 0.1 : 0});
-    transition: 2s;
+    transform: scale(${({ $mode }) => $mode ? 0.1 : 0});
+    transition: ${({ duration }) => duration ? 2 : 10}s;
   }
   &:hover::before {
     transform: scale(1);
@@ -54,6 +54,7 @@ const GlovingPage = () => {
         w: Math.floor(window.innerWidth / 30)
     });
     const [mode, setMode] = useState(true);
+    const [duration, setDuration] = useState(true);
 
     useEffect(() => {
         const debouncedOnResize = debounce(() => {
@@ -66,6 +67,10 @@ const GlovingPage = () => {
             if (keyCode === 113) {
                 setMode(prevMode => !prevMode);
             }
+            if (keyCode === 119) {
+                setDuration(prevDuration => !prevDuration);
+            }
+            console.log(keyCode);
         };
 
         window.addEventListener('resize', debouncedOnResize, false);
@@ -75,13 +80,13 @@ const GlovingPage = () => {
             window.removeEventListener('keypress', onPress, false);
         };
     });
-    console.log(mode);
+
     return (
         <Page>
             {[...Array(numberDots.h)].map((_, i) =>
                 <Row key={i}>
                     {[...Array(numberDots.w)].map((_, j) =>
-                        <Dot amode={mode} key={j}/>
+                        <Dot duration={duration} $mode={mode} key={j}/>
                     )}
                 </Row>
             )}
