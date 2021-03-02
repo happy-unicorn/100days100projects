ad7892b41f85d81d43992fdfcc6fa29f3f283d68
Michael Chernomazov <michael23black@yandex.ru>
2021-03-03 02:45:24 +0300

    Added color picker

--- src/glassmorphism-generator/components/glass/index.js
+++ src/glassmorphism-generator/components/glass/index.js
@@ -13,7 +13,7 @@ const Wrapper = styled.div`
   width: 100%;
   margin-bottom: 25px;
   border-radius: 10px;
-  background: rgba(240, 248, 255, ${({ transparency }) => transparency});
+  background: rgba(${({ color: { r, g, b } }) => `${r}, ${g}, ${b}, `}${({ transparency }) => transparency});
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(${({ blur }) => blur}px);
   
@@ -27,14 +27,14 @@ const Text = styled.div`
   text-shadow: 0 2px 4px rgba(13, 22, 35, 0.3);
 `;
 
-const Glass = ({ blur, transparency }) => {
+const Glass = ({ blur, transparency, color }) => {
   return (
     <Component>
       <Circle size={50} left={'50%'} top={'10%'}/>
       <Circle size={100} left={'20%'} bottom={'20%'}/>
       <Circle size={200} right={'-75px'} bottom={'-25px'}/>
       <Circle after size={150} top={'-75px'} left={'-50px'}/>
-      <Wrapper blur={blur} transparency={transparency}>
+      <Wrapper color={color} blur={blur} transparency={transparency}>
         <Text>
           CSS Glassmorphism Generator
         </Text>
--- src/glassmorphism-generator/containers/Generator.js
+++ src/glassmorphism-generator/containers/Generator.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Slider from '../components/slider';
 import Glass from '../components/glass';
 import Output from '../components/Output';
+import Picker from '../components/Picker';
 
 const Container = styled.div`
   display: flex;
@@ -24,10 +25,9 @@ const SliderWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  align-self: start;
-  height: 45%;
+  height: 90%;
   width: 35%;
-  margin: 10px;
+  padding: 10px;
 `;
 const OutputWrapper = styled.div`
   height: 90%;
@@ -38,6 +38,12 @@ const OutputWrapper = styled.div`
 const Generator = () => {
   const [blur, setBlur] = useState(5.0);
   const [transparency, setTransparency] = useState(0.25);
+  const [color, setColor] = useState({
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 1
+  });
 
   const onBlurChange = useCallback((value) => {
     setBlur(value.toFixed(1));
@@ -45,18 +51,22 @@ const Generator = () => {
   const onTransparencyChange = useCallback((value) => {
     setTransparency(value.toFixed(2));
   }, []);
+  const onColorChange = useCallback(({ rgb }) => {
+    setColor(rgb);
+  }, []);
 
   const code = useMemo(() => {
-    return `background: rgba(255, 255, 255, ${transparency});\nbox-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);\nbackdrop-filter: blur(${blur}px);`;
-  }, [blur, transparency]);
+    return `background: rgba(${color.r}, ${color.g}, ${color.b}, ${transparency});\nbox-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);\nbackdrop-filter: blur(${blur}px);`;
+  }, [blur, transparency, color]);
 
   return (
     <Container>
-      <Glass blur={blur} transparency={transparency}/>
+      <Glass color={color} blur={blur} transparency={transparency}/>
       <Menu>
         <SliderWrapper>
           <Slider text={'Blur'} value={blur} max={20} step={0.5} defaultValue={5.0} onChange={onBlurChange}/>
           <Slider text={'Transparency'} value={transparency} defaultValue={0.25} onChange={onTransparencyChange}/>
+          <Picker onColorChange={onColorChange} color={color}/>
         </SliderWrapper>
         <OutputWrapper>
           <Output code={code}/>
